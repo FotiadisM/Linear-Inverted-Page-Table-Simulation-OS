326d4b4c965e405555fea027eb081c46d6362f43
FotiadisM <fotiadis.michalis20@gmail.com>
2020-01-09 20:44:24 +0200

    reading addresses for files, need to implement lru and ws

---
+++ include/Simulator.h
@@ -0,0 +1,10 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include "./InvertedPageTable.h"
+
+int Simulator_run(char* algorithm, int frames, int quantum, int maxReferences);
+
+Address *Simulator_getAddress(FILE *filePtr1, FILE *filePtr2, int *currReferences, int maxReferences, int *currQuantum, int quantum, bool *togglefiles);
+
+#endif
